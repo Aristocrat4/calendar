c6c1a1dc4a0a2fffbd6c6589d6f20fcd3e4cdb39
GDanelia <galaktion.danelia@space.ge>
2025-01-21 20:22:12 +0400

    add color styling to slider when clicking

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -1,4 +1,4 @@
-import { DatePipe } from '@angular/common';
+import { CommonModule, DatePipe } from '@angular/common';
 import { Component, computed, signal } from '@angular/core';
 
 import {
@@ -10,11 +10,16 @@ import {
 @Component({
   selector: 'app-slider',
   standalone: true,
-  imports: [DatePipe],
+  imports: [DatePipe, CommonModule],
   templateUrl: './slider.component.html',
   styleUrl: './slider.component.scss',
 })
 export class SliderComponent {
+  selectedIndex = signal<number | null>(null);
+
+  onDate(index: number) {
+    this.selectedIndex.set(index);
+  }
   fixedToday = signal(new Date());
   today = signal(new Date());
   endDate = computed(() => addDays(this.today(), 6));
@@ -37,9 +42,11 @@ export class SliderComponent {
   }
   onNextWeek() {
     this.today.set(addDays(this.today(), 7));
+    this.selectedIndex.set(null);
   }
 
   onPreviousWeek() {
     this.today.set(subDays(this.today(), 7));
+    this.selectedIndex.set(null);
   }
 }
